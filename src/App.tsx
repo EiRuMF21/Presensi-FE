655f50881bbbd30171283c74c456cee7044ec7f3
EiRuMF21 <imffarrel@gmail.com>
2024-08-09 08:41:38 +0700

    FE-MANTAP

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "./pages/Login";
+import "./index.css";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+
+const Login: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-sky-500 px-4 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md p-6 space-y-8 bg-neutral-50 rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold text-center text-neutral-900">
+          Login
+        </h2>
+        <form className="mt-8 space-y-6">
+          <div className="rounded-md shadow-sm space-y-4">
+            <div>
+              <label htmlFor="email-address" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-neutral-50 bg-slate-600 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                placeholder="Email address"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-neutral-50 bg-slate-600 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                className="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500"
+              />
+              <label
+                htmlFor="remember-me"
+                className="block ml-2 text-sm text-gray-900"
+              >
+                Remember me
+              </label>
+            </div>
+            <div className="text-sm">
+              <a
+                href="#"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Forgot your password?
+              </a>
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
