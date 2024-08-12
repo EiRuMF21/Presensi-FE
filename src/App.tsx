5043ff363fafde92b20eb069c2304f0f7332e6f9
EiRuMF21 <imffarrel@gmail.com>
2024-08-12 14:16:50 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -14,5 +14,5 @@ const App: React.FC = () => {
     </Router>
   );
 };
-
+  
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,41 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect, useRef } from "react";
 
-const Home: React.FC = () => {  
+const Home: React.FC = () => {
   return (
-    
-    <div className="flex justify-center min-h-screen bg-indigo-700 px-4 sm:px-6 lg:px-8">
-      <div className="w-full max-w-sm p-8 space-y-8">
-        <h1 className="mt-6 text-2xl text-neutral-100 font-bold text-center">
+    <div className="flex flex-col min-h-screen bg-indigo-700 px-6 sm:px-6 lg:px-8">
+      {/* Bagian atas */}
+      <div className="p-8">
+        <h1 className="text-2xl text-neutral-100 font-bold text-center">
           Smart Presence
         </h1>
-        <div>
-          <h2 className="text-center text-4xl text-neutral-100 font-bold ">
-            Digital <br /> Platfrom For
-          </h2>
-          <h2 className="text-center text-4xl text-gray-400 font-bold ">
-            All Employee
-          </h2>
-          <h2 className="text-3xl font-bold text-left text-indigo-700">
-            Smart Presence
-          </h2>
-          <p className="text-left font-inter text-neutral-100">
-            Web Presensi yang praktis dan akurat, memudahkan pencatatan
-            kehadiran secara real-time dengan fitur rekap otomatis dan
-            notifikasi. Solusi presensi digital yang aman untuk meningkatkan
-            efisiensi organisasi Anda.
-          </p>
-        </div>
-        <div className="flex justify-center space-x-4 mt-4">
-          <a href="#" className="text-indigo-600 text-xl">
-            <i className="fab fa-instagram"></i>
-          </a>
-          <a href="#" className="text-indigo-600 text-xl">
-            <i className="fab fa-facebook"></i>
-          </a>
-          <a href="#" className="text-indigo-600 text-xl">
-            <i className="fab fa-twitter"></i>
-          </a>
+      </div>
+
+      {/* Bagian tengah */}
+      <div className="flex-grow flex flex-col justify-center">
+        <div className="w-full max-w-sm p-8 space-y-8">
+          <div>
+            <h2 className="text-center text-4xl text-neutral-100 font-bold">
+              Digital <br /> Platform For
+            </h2>
+            <h2 className="text-center text-4xl text-gray-400 font-bold">
+              All Employee
+            </h2>
+            <br />
+            <p className="text-center font-inter text-neutral-100">
+              Web Presensi yang praktis dan akurat, memudahkan pencatatan
+              kehadiran secara real-time dengan fitur rekap otomatis dan
+              notifikasi. Solusi presensi digital yang aman untuk meningkatkan
+              efisiensi organisasi Anda.
+            </p>
+          </div>
+          <div className="flex justify-center space-x-4 mt-4">
+            <a href="#" className="text-indigo-600 text-xl">
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a href="#" className="text-indigo-600 text-xl">
+              <i className="fab fa-facebook"></i>
+            </a>
+            <a href="#" className="text-indigo-600 text-xl">
+              <i className="fab fa-twitter"></i>
+            </a>
+          </div>
         </div>
       </div>
     </div>
