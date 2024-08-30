48e4a5004e90953f76d1cd4f26e73ae38498edc8
EiRuMF21 <imffarrel@gmail.com>
2024-08-30 09:21:17 +0700

    Update

--- src/components/SidebarAdmin.tsx
+++ src/components/SidebarAdmin.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SidebarAdmin: React.FC = () => {
   return (
-    <div className="bg-[#141414] text-white h-screen w-64 p-4">
+    <div className="bg-[#05073C] text-white h-screen w-64 p-4 rounded-2xl">
       <h2 className="text-white font-semibold text-lg mb-4">USER MANAGEMENT</h2>
       <button className="bg-[#29b6f6] text-white py-2 px-4 rounded w-full mb-4">
         User Data
--- src/pages/DashboardAdmin.tsx
+++ src/pages/DashboardAdmin.tsx
@@ -1,15 +1,32 @@
 import React from "react";
 import NavbarAdmin from "../components/NavbarAdmin";
 import SidebarAdmin from "../components/SidebarAdmin";
+import AttendanceChart from "../components/AttendanceChart";
+import PermissionChart from "../components/SickChart";
+import SickChart from "../components/SickChart";
+import HolidayChart from "../components/HolidayChart";
+import ServiceChart from "../components/ServiceChart";
 
 const DashboardAdmin: React.FC = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen drop-shadow-2xl">
       <NavbarAdmin />
-      <div className="flex flex-grow bg-white">
+      <div className="flex flex-grow bg-white mt-9">
         <SidebarAdmin />
         <div className="flex-grow p-4 bg-[#cfcfcf]">
-          {/* Your main content goes here */}
+          <AttendanceChart />
+          <div className="mt-10">
+            <PermissionChart />
+          </div>
+          <div className="mt-10">
+            <SickChart />
+          </div>
+          <div className="mt-10">
+            <HolidayChart />
+          </div>
+          <div className="mt-10">
+            <ServiceChart />
+          </div>
         </div>
       </div>
     </div>
