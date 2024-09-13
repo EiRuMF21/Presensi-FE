7eb84fbd69454fa06a58521cb75c440d15832812
EiRuMF21 <imffarrel@gmail.com>
2024-09-13 08:36:28 +0700

    iurie

--- src/components/NavbarAdmin.tsx
+++ src/components/NavbarAdmin.tsx
@@ -4,10 +4,8 @@ const NavbarAdmin: React.FC = () => {
   return (
     <div className="w-full bg-white mx-auto px-4 sm:px-6 lg:px-8 drop-shadow-2xl">
       <div className="flex items-center justify-between h-16">
-        <div className="flex items-center">
-          <a className="text-lg font-bold tracking-widest text-[#212121] font-sans">
-            ADMIN SMART ATTENDANCE
-          </a>
+        <div className="flex items-center w-20">
+          <img src="public/image/logoSA.png" alt="" />
         </div>
       </div>
     </div>
